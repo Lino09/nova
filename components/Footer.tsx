66651fa5a262e0fa3e22a187618ed5a6e4d285c3
Lino09 <onosmx@gmail.com>
2025-02-10 17:00:11 -0600

    form fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="text-cyberpunk-wht py-8 bg-cyberpunk-blk flex justify-center mt-16 border-t px-4">
+    <footer className="text-cyberpunk-wht p-8 bg-cyberpunk-blk flex justify-center mt-16 border-t px-4">
       <div className="flex gap-8 flex-wrap justify-center">
         <Link href="mailto:emmanueliandenova@hotmail.com" className="hover:text-cyberpunk-ylw transition-colors duration-300">
           emmanueliandenova@hotmail.com
