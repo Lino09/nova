4a53fcb48c095159168a9ad80d6420c2409546f7
Lino09 <onosmx@gmail.com>
2025-02-10 18:04:35 -0600

    safari fix

--- components/GlitchText.tsx
+++ components/GlitchText.tsx
@@ -6,36 +6,32 @@ export default function GlitchText({children, className}:{children: string, clas
   const textRef = useRef(null);
 
   useEffect(() => {
-  
-
     const glitchTimeline = gsap.timeline({
       repeat: -1,
-      repeatDelay:  Math.floor(Math.random() *6) + 1
+      repeatDelay: Math.floor(Math.random() * 6) + 1
     });
 
-    glitchTimeline.to('.glitch', {duration: 0.1, skewX: '60',  ease: "power4.inOut"})
-    .to('.glitch', {duration: 0.05, skewX: '0' })
-    .to('.glitch', {duration: 0.04, opacity:0})
-    .to('.glitch', {duration: 0.04, opacity:1})
-    .to('.glitch', {duration: 0.04, x:-20})
-    .to('.glitch', {duration: 0.04, x:0})
-    .add("split", 0)
-    .to('.top', {duration: 0.5, x:-10,ease: 'Power4.easeInOut'},'split')
-    .to('.top', {duration: 0.5, x:0,ease: 'Power4.easeInOut'},'split')
-    .to('.top', {duration: 0.5, x:0,ease: 'Power4.easeInOut'},'split')
-    .to('.bottom', {duration: 0.5, x:0,ease: 'Power4.easeInOut'},'split')
-    .to('.glitch', {duration: 0.08,  'text-shadow': '-2px 0 red'},'split')
-    .to('#txt', {duration: 0,  scale:1.1},'split')
-    .to('#txt', {duration: 0,  scale:1}, "+=0.02")
-    .to('.glitch', {duration: 0.08, 'text-shadow': 'none'}, "+=0.09")
-    .to('.glitch', {duration: 0.03, 'text-shadow': '-3px 0 green'},'split')
-    .to('.glitch', {duration: 0.03, 'text-shadow': 'none'},"+=0.1")
-    .to('.top', {duration: 0.2, x:0,ease: 'Power4.easeInOut'})
-    .to('.bottom', {duration: 0.2, x:0,ease: 'Power4.easeInOut'})
-    .to('.glitch', {duration: 0.02, scaleY:1.1,ease: 'Power4.easeInOut'})
-    .to('.glitch', {duration: 0.04, scaleY:1,ease: 'Power4.easeInOut'})
-  
-
+    glitchTimeline.to('.glitch', {duration: 0.1, skewX: '60', ease: "power4.inOut"})
+      .to('.glitch', {duration: 0.05, skewX: '0'})
+      .to('.glitch', {duration: 0.04, opacity: 0})
+      .to('.glitch', {duration: 0.04, opacity: 1})
+      .to('.glitch', {duration: 0.04, x: -20})
+      .to('.glitch', {duration: 0.04, x: 0})
+      .add("split", 0)
+      .to('.top', {duration: 0.5, x: -10, ease: 'Power4.easeInOut'}, 'split')
+      .to('.top', {duration: 0.5, x: 0, ease: 'Power4.easeInOut'}, 'split')
+      .to('.top', {duration: 0.5, x: 0, ease: 'Power4.easeInOut'}, 'split')
+      .to('.bottom', {duration: 0.5, x: 0, ease: 'Power4.easeInOut'}, 'split')
+      .to('.glitch', {duration: 0.08, 'text-shadow': '-2px 0 red'}, 'split')
+      .to('#txt', {duration: 0, scale: 1.1}, 'split')
+      .to('#txt', {duration: 0, scale: 1}, "+=0.02")
+      .to('.glitch', {duration: 0.08, 'text-shadow': 'none'}, "+=0.09")
+      .to('.glitch', {duration: 0.03, 'text-shadow': '-3px 0 green'}, 'split')
+      .to('.glitch', {duration: 0.03, 'text-shadow': 'none'}, "+=0.1")
+      .to('.top', {duration: 0.2, x: 0, ease: 'Power4.easeInOut'})
+      .to('.bottom', {duration: 0.2, x: 0, ease: 'Power4.easeInOut'})
+      .to('.glitch', {duration: 0.02, scaleY: 1.1, ease: 'Power4.easeInOut'})
+      .to('.glitch', {duration: 0.04, scaleY: 1, ease: 'Power4.easeInOut'});
 
     return () => {
       glitchTimeline.kill();
@@ -43,16 +39,16 @@ export default function GlitchText({children, className}:{children: string, clas
   }, []);
 
   return (
-  <h1 ref={textRef} className={`txt relative ${className}`} >
-    <span className='top glitch absolute'>
-      {children}
-    </span>
-    <span className='bottom glitch absolute'>
-      {children}
-    </span>
-    <span className='invisible'>
-      {children}
-    </span>
-  </h1>
+    <h1 ref={textRef} className={`txt relative ${className} whitespace-nowrap`}>
+      <span className='top glitch absolute whitespace-nowrap'>
+        {children}
+      </span>
+      <span className='bottom glitch absolute whitespace-nowrap'>
+        {children}
+      </span>
+      <span className='invisible'>
+        {children}
+      </span>
+    </h1>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,23 +1,21 @@
 import Link from "next/link";
-import {orbitron, shareTechMono} from '@/app/layout'
-import {GlitchText, CyberButton} from "@/components";
+import { orbitron, shareTechMono } from '@/app/layout';
+import { GlitchText, CyberButton } from "@/components";
 
 const Header = () => (
-<header className={`${shareTechMono.className} text-base wide-parent`} id="tops">
-  <nav className="border px-4 py-2 flex section rounded-sm mt-4 border-cyberpunk-ylw justify-between">
-    <div className={`${orbitron.className} gap-4 sm:gap-8 md:justify-between text-sm w-full flex flex-wrap justify-center`}>
-      <Link href="#tops" className="w-full sm:w-auto mx-auto sm:mx-0 text-center">
-        <GlitchText className="inline">Ian De Nova</GlitchText>    
-      </Link>
-      <Link href="/#aboutme">Sobre Mi</Link>
-      <Link href="/#proyects">Proyectos</Link>
-      {/* <Link href="/#contact">Contacto</Link> */}
-    <Link href="/#contact" className="">
-      <CyberButton>Contacto</CyberButton>    
-    </Link>
-    </div>
-
-  </nav>
-</header>
-)
+  <header className={`${shareTechMono.className} text-base wide-parent`} id="tops">
+    <nav className="border px-4 py-2 flex section rounded-sm mt-4 border-cyberpunk-ylw justify-between">
+      <div className={`${orbitron.className} gap-4 sm:gap-8 md:justify-between text-sm w-full flex flex-wrap justify-center`}>
+        <Link href="#tops" className="w-full sm:w-auto mx-auto sm:mx-0 text-center">
+          <GlitchText className="inline text-2xl md:text-4xl">Ian De Nova</GlitchText>
+        </Link>
+        <Link href="/#aboutme">Sobre Mi</Link>
+        <Link href="/#proyects">Proyectos</Link>
+        <Link href="/#contact" className="">
+          <CyberButton>Contacto</CyberButton>
+        </Link>
+      </div>
+    </nav>
+  </header>
+);
 export default Header;
